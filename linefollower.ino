f4c9423e19d33b2065f17a4626bb9c87e4067d51
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2019-05-23 15:41:33 +0200

    Code refactoring and preparing to implement pd controller

--- linefollower.ino
+++ linefollower.ino
@@ -1,73 +1,74 @@
-//TODO: ultrasonic
+// Ultrasonic
 #define TRIGGER 12
 #define ECHO    13
-//ENABLE_L
-#define ENABLE1 10
-//ENABLE_R
-#define ENABLE2 11
-#define MOTORL1 7
-#define MOTORL2 6
-#define MOTORR1 8
-#define MOTORR2 9
-const int Speed = 180;
-const int Speed_turn = 150;
-const int Speed_turn_fast = 180;
+// Left motor
+#define ENABLE_LEFT 10
+#define MOTOR_LEFT1 7
+#define MOTOR_LEFT2 6
+// Right motor
+#define ENABLE_RIGHT 11
+#define MOTOR_RIGHT1 8
+#define MOTOR_RIGHT2 9
+// Sensors
 #define NUMBER_OF_SENSORS 5
 static const uint8_t sensor_pins[] = {A0, A1, A2, A3, A4};
+// Regulators
+#define KP 50
+#define TD 50
+
+#define BASE_SPEED 180
 enum DIRECTION {FORWARD, BACKWARD};
-typedef unsigned char BYTE; // Unsigned char holds 1 byte of data
-int last_sensor;
-const int us_delay = 10;
-int iteration;
+int last_sensor; // Index of the last sensor that've detected a line
 
-/* speed - 0-255
-   direction - FORWARD, BACKWARD
-*/
-void left_motor(BYTE speed, enum DIRECTION direction) {
+void left_motor(byte speed, enum DIRECTION direction) {
+  /*
+  Moves the left motor
+  speed - 0-255
+  direction - FORWARD, BACKWARD
+  */
   switch (direction) {
-    case FORWARD:  digitalWrite(MOTORL2, LOW); digitalWrite(MOTORL1, HIGH); break;
-    case BACKWARD: digitalWrite(MOTORL1, LOW); digitalWrite(MOTORL2, HIGH); break;
-    default:       Serial.println("Error: wrong direction!");               break;
+    case FORWARD:  digitalWrite(MOTOR_LEFT2, LOW); digitalWrite(MOTOR_LEFT1, HIGH); break;
+    case BACKWARD: digitalWrite(MOTOR_LEFT1, LOW); digitalWrite(MOTOR_LEFT2, HIGH); break;
   }
-  analogWrite(ENABLE1, speed);
+  analogWrite(ENABLE_LEFT, speed);
 }
 
-void right_motor(BYTE speed, enum DIRECTION direction) {
+void right_motor(byte speed, enum DIRECTION direction) {
+  /* 
+  Moves the right motor
+  speed - 0-255
+  direction - FORWARD, BACKWARD
+  */
   switch (direction) {
-    case FORWARD:  digitalWrite(MOTORR1, LOW); digitalWrite(MOTORR2, HIGH); break;
-    case BACKWARD: digitalWrite(MOTORR2, LOW); digitalWrite(MOTORR1, HIGH); break;
-    default:       Serial.println("Error: wrong direction!");               break;
+    case FORWARD:  digitalWrite(MOTOR_RIGHT1, LOW); digitalWrite(MOTOR_RIGHT2, HIGH); break;
+    case BACKWARD: digitalWrite(MOTOR_RIGHT2, LOW); digitalWrite(MOTOR_RIGHT1, HIGH); break;
   }
-  analogWrite(ENABLE2, speed);
+  analogWrite(ENABLE_RIGHT, speed);
 }
 
+void ride(int error, int last_error) {
+  /*
+  Moves the motors according to the PD controller
+  error - Current error value
+  last_error - Error value from the last iteration
+  */
+}
 
-/* Reads values of sensors
-*/
-int read_sensor_values(int *value_array) {
-  for (BYTE i = 0; i < NUMBER_OF_SENSORS; ++i) {
+void read_sensor_values(int *value_array) {
+  /* 
+  Reads values of sensors and stores them in value_array
+  value_array - a pointer to the array
+  */
+  for (byte i = 0; i < NUMBER_OF_SENSORS; ++i) {
     value_array[i] = analogRead(sensor_pins[i]);
   }
-  return 0; // Just for now
-}
-
-int get_minimum_sensor(int *values) {
-  int minimum_val = 2000;
-  int minimum_sensor;
-  for (int i = 0; i < NUMBER_OF_SENSORS; ++i) {
-    Serial.print(values[i], DEC);
-    Serial.print(", ");
-    if (values[i] < minimum_val && values[i] < 600) {
-      minimum_val = values[i];
-      minimum_sensor = i;
-    }
-  }
-  Serial.print('\n');
-  if (minimum_val == 2000) return -1; // Jeśli nie znaleziono min, to zwraca -1
-  else return minimum_sensor;
 }
 
 int measure_distance() {
+  /* 
+  Returns distance in cm read from an ultrasonic sensor
+  return - distance in cm
+  */
   // https://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/
   long duration;
   int distance;
@@ -87,93 +88,60 @@ int measure_distance() {
 }
 
 void avoid() {
-  left_motor(Speed_turn,FORWARD); right_motor(0,FORWARD);
+  /* 
+  Performs a preprogrammed series of moves to avoid an obstacle
+  */
+  left_motor(BASE_SPEED,FORWARD); right_motor(0,FORWARD);
   delay(500);
-  left_motor(Speed,FORWARD); right_motor(Speed,FORWARD);
+  left_motor(BASE_SPEED,FORWARD); right_motor(BASE_SPEED,FORWARD);
   delay(300);
-  left_motor(0,FORWARD); right_motor(Speed_turn,FORWARD);
+  left_motor(0,FORWARD); right_motor(BASE_SPEED,FORWARD);
   delay(500);
   
-  left_motor(Speed,FORWARD); right_motor(Speed,FORWARD);
+  left_motor(BASE_SPEED,FORWARD); right_motor(BASE_SPEED,FORWARD);
   delay(600);
   
-  left_motor(0,FORWARD); right_motor(Speed_turn,FORWARD);
+  left_motor(0,FORWARD); right_motor(BASE_SPEED,FORWARD);
   delay(500);
-  left_motor(Speed,FORWARD); right_motor(Speed,FORWARD);
+  left_motor(BASE_SPEED,FORWARD); right_motor(BASE_SPEED,FORWARD);
   delay(300);
-  left_motor(Speed_turn,FORWARD); right_motor(0,FORWARD);
+  left_motor(BASE_SPEED,FORWARD); right_motor(0,FORWARD);
   delay(500);
 }
 
-void test() {
-  //digitalWrite(MOTORL2, LOW);
-  //digitalWrite(MOTORL1, HIGH);
-  //analogWrite(ENABLE1, 150);
-  left_motor(255, FORWARD);
-  right_motor(255, FORWARD);
-}
-bool was_overflow;
 void setup() { // Runs once on boot
-  
-  // Setup ultrasonic sensor pins
+  // Setup the ultrasonic sensor pins
   pinMode(TRIGGER, OUTPUT);
   pinMode(ECHO, INPUT);
-  // Setup motor pins
-  pinMode(ENABLE1, OUTPUT);
-  pinMode(ENABLE2, OUTPUT);
-  pinMode(MOTORL1, OUTPUT);
-  pinMode(MOTORL2, OUTPUT);
-  pinMode(MOTORR1, OUTPUT);
-  pinMode(MOTORR2, OUTPUT);
+  // Setup motors pins
+  pinMode(ENABLE_LEFT, OUTPUT);
+  pinMode(MOTOR_LEFT1, OUTPUT);
+  pinMode(MOTOR_LEFT2, OUTPUT);
+  pinMode(ENABLE_RIGHT, OUTPUT);
+  pinMode(MOTOR_RIGHT1, OUTPUT);
+  pinMode(MOTOR_RIGHT2, OUTPUT);
   // Turn off the motors
-  digitalWrite(MOTORL1, LOW);
-  digitalWrite(MOTORL2, LOW);
-  digitalWrite(MOTORR1, LOW);
-  digitalWrite(MOTORR2, LOW);
-  // Setup sensor pins
-  for (BYTE i = 0; i < NUMBER_OF_SENSORS; ++i) {
+  digitalWrite(MOTOR_LEFT1, LOW);
+  digitalWrite(MOTOR_LEFT2, LOW);
+  digitalWrite(MOTOR_RIGHT1, LOW);
+  digitalWrite(MOTOR_RIGHT2, LOW);
+  // Setup sensors pins
+  for (byte i = 0; i < NUMBER_OF_SENSORS; ++i) {
     pinMode(sensor_pins[i], INPUT);
   }
   // Initialize serial connection
   Serial.begin(115200);
-  last_sensor = 1;
-  iteration = 0;
-  was_overflow = false;
+  last_sensor = 2; // The last sensor is the middle one
 }
 
-int distance=0;
-
 void loop() { // Infinite loop
-  for (;;) {
     int values[NUMBER_OF_SENSORS];
+    int minimum_sensor;
     read_sensor_values(values);
-    int minimum_sensor = get_minimum_sensor(values);
-    if(iteration==us_delay) {
-          distance = measure_distance();
-  
-        iteration = 0;
-  
-        if (distance < 30 && distance > 10) {
-          avoid();
-          continue;
-        }
-    }
-    //test();
-    Serial.println(distance, DEC);
-    Serial.println("\n");
     if(minimum_sensor == -1) {
       minimum_sensor = last_sensor;
     }
-    
-    switch(minimum_sensor) {
-      case 0: left_motor(0,FORWARD); right_motor(Speed_turn_fast,FORWARD); break;
-      case 1: left_motor(0,FORWARD); right_motor(Speed_turn,FORWARD);  break;
-      case 2: left_motor(Speed,FORWARD); right_motor(Speed,FORWARD); break;
-      case 3: left_motor(Speed_turn,FORWARD);  right_motor(0,FORWARD);  break;
-      case 4: left_motor(Speed_turn_fast,FORWARD); right_motor(0,FORWARD); break;
-    }
+
+
     last_sensor = minimum_sensor;
-    //delay(100);
-    ++iteration;
-  }
 }
